6fecb732ae284567267cc7b2f2f9e7f1996a2697
Mounir-mk <mounir.mekoui@gmail.com>
2023-05-27 22:58:29 +0200

    modifying cors policy

--- src/app.js
+++ src/app.js
@@ -5,13 +5,26 @@ const router = require("./router");
 const app = express();
 
 // use some application-level middlewares
+let allowedOrigins = ["http://localhost:3000"];
+
+if (process.env.NODE_ENV === "production") {
+  allowedOrigins = [process.env.FRONTEND_URL];
+}
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
-    optionsSuccessStatus: 200,
+    // eslint-disable-next-line object-shorthand, func-names
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "La politique CORS ne permet pas l'accès à cette ressource pour cette origine spécifique.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
-
 app.use(express.json());
 
 // Serve the uploads folder for uploaded resources
